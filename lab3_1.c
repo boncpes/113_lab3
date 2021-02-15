316a31f19e409644e6a6684ab476328ec85192fd
boncpes <78350722+boncpes@users.noreply.github.com>
2021-02-15 16:56:27 +0700

    loop for information

--- lab3_1.c
+++ lab3_1.c
@@ -1,6 +1,28 @@
 #include<stdio.h>
-int main(){
-
-
-return 0;
+int plus(int num[5])
+{
+    int sumz=0;
+    for(i=0; i<5; i++)
+    {
+        sumz+=num[i]
+    }
+    return sumz;
+}
+int main()
+{
+    int num[5],sum[6],end=-1;
+    while(end<6)
+    {
+        for(i=0; i<5; i++)
+        {
+            scanf("%d",num[i]);
+        }
+        end++;
+        sum[end]=plus(num);
+    }
+    for(i=0; i<5; i++)
+    {
+        if(sum[i])
+    }
+    return 0;
 }
