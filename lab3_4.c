ba3d75cf2a3a39a6575dc74df8d6e24f1247bae3
boncpes <78350722+boncpes@users.noreply.github.com>
2021-02-15 19:57:17 +0700

    lab4 fibnal

--- lab3_4.c
+++ lab3_4.c
@@ -1,11 +1,32 @@
 #include<stdio.h>
+unsigned int p(unsigned int x)
+{
+    unsigned int c=0,a;
+    for(a=2;a<=x/2;a++)
+    {
+        if(x%a==0)
+        {
+            c++;
+        }
+    }
+    return c;
+}
 int main()
 {
-    long x,a;
-    scanf("%ld,&x);
-    for(a=2;a<=n;a++)
+    unsigned int x,c=0;
+    scanf("%u",&x);
+    while(1)
     {
-        if(x
+        c=p(x);
+        if(c==0)
+        {
+            printf("%u",x);
+            break;
+        }
+        else
+        {
+            x--;
+        }
     }
     return 0;
 }
